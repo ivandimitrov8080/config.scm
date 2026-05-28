d165f70c84f53865ef87fe5ea53ef1337f69bc94
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2026-05-28 10:58:54 +0200

    cleanup

--- systems/common.scm
+++ systems/common.scm
@@ -78,7 +78,7 @@
 ;; ---------------------------------------------------------------------------
 
 (define-public %my-keyboard-layout
-  (keyboard-layout "us,bg" "phonetic"
+  (keyboard-layout "us,bg" ",phonetic"
                    #:options '("grp:win_space_toggle")))
 
 ;; ---------------------------------------------------------------------------
@@ -288,19 +288,7 @@
       ;; Behaviour
       "focus_follows_mouse no"
       "mouse_warping none"
-      ;; Resize mode
-      "mode \"resize\" {"
-      "  bindsym h resize shrink width 10px"
-      "  bindsym j resize grow height 10px"
-      "  bindsym k resize shrink height 10px"
-      "  bindsym l resize grow width 10px"
-      "  bindsym Left resize shrink width 10px"
-      "  bindsym Down resize grow height 10px"
-      "  bindsym Up resize shrink height 10px"
-      "  bindsym Right resize grow width 10px"
-      "  bindsym Return mode \"default\""
-      "  bindsym Escape mode \"default\""
-      "}"))))
+      ))))
 
 ;; ---------------------------------------------------------------------------
 ;; Desktop home environment
