dc46b21e0e9979be9323a492b888538b6a691f84
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2026-07-09 14:26:40 +0200

    cleanup

--- shell.scm
+++ shell.scm
@@ -1,38 +1,33 @@
 ;; pwntools development environment
 ;; Usage: guix shell -m shell.scm -- xonsh
 (specifications->manifest
- '(;; core CTF framework (brings capstone, unicorn, ropgadget, pyelftools,
-   ;; paramiko, requests, rpyc, psutil, etc. transitively)
+ '(
    "pwntools"
    "python"
 
-   ;; shell
-   "xonsh"                     ; Python-ish interactive shell
+   "xonsh"
 
-   ;; crypto / math
-   "python-pycryptodome"       ; AES, RSA, DES, hashing, padding oracles
-   "python-cryptography"       ; higher-level crypto primitives
-   "python-gmpy2"              ; GMP big-integer math (RSA/DLP/factoring)
-   "python-sympy"              ; symbolic math, modular arithmetic, factoring
+   "python-pycryptodome"
+   "python-cryptography"
+   "python-gmpy2"
+   "python-sympy"
 
-   ;; numeric / interactive
-   "python-numpy"              ; arrays, struct packing helpers
-   "python-ipython"            ; enhanced REPL with tab-completion
 
-   ;; debugging & reverse engineering
-   "gdb-multiarch"             ; debugger with multi-arch support
-   "radare2"                   ; disassembler / RE framework
+   "python-numpy"
+   "python-ipython"
 
-   ;; tracing
-   "strace"                    ; syscall tracing
-   "ltrace"                    ; library call tracing
 
-   ;; assembler / binary utilities
-   "nasm"                      ; x86/x64 assembler
-   "binutils"                  ; objdump, readelf, strings, nm, objcopy
+   "gdb-multiarch"
+   "radare2"
 
-   ;; networking
-   "netcat"                    ; raw TCP/UDP interaction
 
-   ;; misc
-   "file"))                    ; ELF/binary identification
+   "strace"
+   "ltrace"
+
+   "nasm"
+   "binutils"
+
+
+   "netcat"
+
+   "file"))
